e40733d79a89edfdf545165fbc6b19b7b6e2bc99
Alan Mendoza <alan.ms9715@gmail.com>
2024-02-28 12:16:13 -0500

    Created functional tip calculator

--- scripts.js
+++ scripts.js
@@ -1,24 +1,30 @@
-let userInput;
-userInput = prompt("Choose a number", "Type your answer here.");
+let totalInput;
+totalInput = prompt("What's the bill total?", "Type your answer here.");
 
-$("#response").html("<p>This is the list</p>");
+let tipTotal;
+tipTotal = prompt("How much tip do you want to leave?", "Type your respone here");
 
-for (let i = 1; i <= userInput; i = i + 1) {
+let tipCalculator; // crea varibale 
+tipCalculator = function(){ //define variable
+  let tipAmount;
+  tipAmount = totalInput * tipTotal;
+  if (tipTotal % 10 === 0) {
+    let tipDiv;
+    tipDiv = tipTotal / 100;
+    $("#response").html("Your tip is $" + (totalInput * tipDiv));
+  } else {
+    $("#response").html("Your tip is $" + tipAmount);
+  }
+};
+tipCalculator()
+
+
+/* $("#response").html("<p>This is the list</p>");
+
+for (let i = 1; i <= totalInput; i = i + 1) {
   if (i % 2 === 0) {
       $("#response").append("<br />" + i + " is even");
     } else {
       $("#response").append("<br />" + i + " is odd");
     }
-}
-
-
-let tipCalculator; // crea varibale 
-tipCalculator = function(total, tipRate){ //define variable
-  let tipAmount;
-  tipAmount = tipRate * total;
-
-  $("#response").html("Your tip is $" + tipAmount)
-
-};
-
-tipCalculator(50.00, 0.2)
+} */
