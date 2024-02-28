6537960e1028f50f1f0ebf0fd3d52817be23d24f
Alan Mendoza <alan.ms9715@gmail.com>
2024-02-28 12:19:23 -0500

    added comments to code

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ let tipTotal;
 tipTotal = prompt("How much tip do you want to leave?", "Type your respone here");
 
 let tipCalculator; // crea varibale 
-tipCalculator = function(){ //define variable
+tipCalculator = function(){ //define variable como funcion
   let tipAmount;
   tipAmount = totalInput * tipTotal;
-  if (tipTotal % 10 === 0) {
-    let tipDiv;
-    tipDiv = tipTotal / 100;
+  if (tipTotal % 10 === 0) { // test si el numero introducido tiene un remainder de 0
+    let tipDiv; // crea nueva variable para numeros enteros
+    tipDiv = tipTotal / 100; // transforma numero introducido en decimal
     $("#response").html("Your tip is $" + (totalInput * tipDiv));
   } else {
     $("#response").html("Your tip is $" + tipAmount);
@@ -21,7 +21,7 @@ tipCalculator()
 
 /* $("#response").html("<p>This is the list</p>");
 
-for (let i = 1; i <= totalInput; i = i + 1) {
+for (let i = 1; i <= totalInput; i = i + 1) { // 'for' es un loop
   if (i % 2 === 0) {
       $("#response").append("<br />" + i + " is even");
     } else {
